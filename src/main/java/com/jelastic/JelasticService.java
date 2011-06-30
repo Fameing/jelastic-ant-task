3e88e729040e2004ef998f1203b88ab3e050f877
igor.yova@gmail.com <igor.yova@gmail.com@6f0acfcb-4e31-cb77-e17d-5ba5b1ded12d>
2011-06-30 09:42:32 +0000

    create project

---
+++ src/main/java/com/jelastic/JelasticService.java
@@ -0,0 +1,242 @@
+package com.jelastic;
+
+import com.jelastic.model.*;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Igor Yova
+ * Date: 6/17/11
+ * Time: 5:27 PM
+ */
+public class JelasticService {
+    private String shema = "http";
+    private String hostApp = "app.hivext.com";
+    private String hostApi = "api.hivext.com";
+    private String hostApiDe = "de-hetzner.hivext.com";
+    private String hostLogs = "178.159.250.35";
+    private String jelastiсAppid = "8129583aae37a4b556d36dbd56abbc68";
+
+    private int port = -1;
+    private String version = "1.0";
+
+    private CookieStore cookieStore = null;
+    private String urlAuthentication = "/" + version + "/users/authentication/rest/signin";
+    private String urlUploader = "/" + version + "/storage/uploader/rest/upload";
+    private String urlCreateObject = "/" + version + "/data/base/rest/createobject";
+    private String urlEval = "/" + version + "/development/scripting/rest/eval";
+    private static ObjectMapper mapper = new ObjectMapper();
+    private Project project;
+
+    public JelasticService(Project project) {
+        this.project = project;
+    }
+
+    public void setProject(Project proj) {
+        project = proj;
+    }
+
+    public String getJelastiсAppid() {
+        return jelastiсAppid;
+    }
+
+    public String getShema() {
+        return shema;
+    }
+
+    public String getHostApp() {
+        return hostApp;
+    }
+
+    public String getHostApi() {
+        return hostApi;
+    }
+
+    public String getHostApiDe() {
+        return hostApiDe;
+    }
+
+    public String getHostLogs() {
+        return hostLogs;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public CookieStore getCookieStore() {
+        return cookieStore;
+    }
+
+    public String getUrlAuthentication() {
+        return urlAuthentication;
+    }
+
+    public String getUrlUploader() {
+        return urlUploader;
+    }
+
+    public String getUrlCreateObject() {
+        return urlCreateObject;
+    }
+
+    public String getUrlEval() {
+        return urlEval;
+    }
+
+    public Authentication authentication(String email, String password) {
+        Authentication authentication = null;
+
+        try {
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+
+            List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+            qparams.add(new BasicNameValuePair("appid", getJelastiсAppid()));
+            qparams.add(new BasicNameValuePair("login", email));
+            qparams.add(new BasicNameValuePair("password", password));
+            URI uri = URIUtils.createURI(getShema(), getHostApp(), getPort(), getUrlAuthentication(), URLEncodedUtils.format(qparams, "UTF-8"), null);
+            project.log(uri.toString(),Project.MSG_DEBUG);
+            HttpGet httpGet = new HttpGet(uri);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            String responseBody = httpclient.execute(httpGet, responseHandler);
+            project.log(responseBody,Project.MSG_DEBUG);
+            authentication = mapper.readValue(responseBody, Authentication.class);
+            cookieStore = httpclient.getCookieStore();
+        } catch (URISyntaxException e) {
+            project.log(e.getMessage());
+        } catch (ClientProtocolException e) {
+            project.log(e.getMessage());
+        } catch (IOException e) {
+            project.log(e.getMessage());
+        }
+        return authentication;
+    }
+
+    public UpLoader upload(String filename, String dir, Authentication authentication) {
+        UpLoader upLoader = null;
+        try {
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            httpclient.setCookieStore(getCookieStore());
+
+            File file = new File(dir + File.separator + filename);
+            if (!file.exists()) {
+                throw new BuildException("First build artifact and try again. Artifact not found in : " + dir + File.separator + filename);
+            }
+
+            MultipartEntity multipartEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+            multipartEntity.addPart("appid", new StringBody(getJelastiсAppid()));
+            multipartEntity.addPart("fid", new StringBody("123456"));
+            multipartEntity.addPart("session", new StringBody(authentication.getSession()));
+            multipartEntity.addPart("file", new FileBody(file));
+
+
+            URI uri = URIUtils.createURI(getShema(), getHostApi(), getPort(), getUrlUploader(), null, null);
+            project.log(uri.toString(),Project.MSG_DEBUG);
+            HttpPost httpPost = new HttpPost(uri);
+            httpPost.setEntity(multipartEntity);
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            project.log(responseBody,Project.MSG_DEBUG);
+            upLoader = mapper.readValue(responseBody, UpLoader.class);
+        } catch (URISyntaxException e) {
+            project.log(e.getMessage());
+        } catch (ClientProtocolException e) {
+            project.log(e.getMessage());
+        } catch (IOException e) {
+            project.log(e.getMessage());
+        }
+        return upLoader;
+    }
+
+    public CreateObject createObject(String filename, UpLoader upLoader, Authentication authentication) {
+        CreateObject createObject = null;
+        try {
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            httpclient.setCookieStore(getCookieStore());
+
+            List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+            nameValuePairList.add(new BasicNameValuePair("charset", "UTF-8"));
+            nameValuePairList.add(new BasicNameValuePair("appid", getJelastiсAppid()));
+            nameValuePairList.add(new BasicNameValuePair("session", authentication.getSession()));
+            nameValuePairList.add(new BasicNameValuePair("type", "JDeploy"));
+            nameValuePairList.add(new BasicNameValuePair("data", "{'name':'" + filename + "', 'archive':'" + upLoader.getFile() + "', 'link':0, 'size':" + upLoader.getSize() + ", 'comment':'" + filename + "'}"));
+
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(nameValuePairList, "UTF-8");
+            URI uri = URIUtils.createURI(getShema(), getHostApp(), getPort(), getUrlCreateObject(), null, null);
+            project.log(uri.toString(), Project.MSG_DEBUG);
+            HttpPost httpPost = new HttpPost(uri);
+            httpPost.setEntity(entity);
+
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            project.log(responseBody, Project.MSG_DEBUG);
+            createObject = mapper.readValue(responseBody, CreateObject.class);
+        } catch (URISyntaxException e) {
+            project.log(e.getMessage());
+        } catch (ClientProtocolException e) {
+            project.log(e.getMessage());
+        } catch (IOException e) {
+            project.log(e.getMessage());
+        }
+        return createObject;
+    }
+
+    public Deploy deploy(String context, String environment, Authentication authentication, UpLoader upLoader, CreateObject createObject) {
+        Deploy deploy = null;
+
+        try {
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            httpclient.setCookieStore(getCookieStore());
+
+            List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+            qparams.add(new BasicNameValuePair("charset", "UTF-8"));
+            qparams.add(new BasicNameValuePair("appid", getJelastiсAppid()));
+            qparams.add(new BasicNameValuePair("session", authentication.getSession()));
+            qparams.add(new BasicNameValuePair("script", "deploy/DeployArchive"));
+            qparams.add(new BasicNameValuePair("archiveUri", upLoader.getFile()));
+            qparams.add(new BasicNameValuePair("archiveName", upLoader.getName()));
+            qparams.add(new BasicNameValuePair("newContext", context));
+            qparams.add(new BasicNameValuePair("domain", environment));
+
+            URI uri = URIUtils.createURI(getShema(), getHostApiDe(), getPort(), getUrlEval(), URLEncodedUtils.format(qparams, "UTF-8"), null);
+            project.log(uri.toString(), Project.MSG_DEBUG);
+            HttpGet httpPost = new HttpGet(uri);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            project.log(responseBody, Project.MSG_DEBUG);
+            deploy = mapper.readValue(responseBody, Deploy.class);
+        } catch (URISyntaxException e) {
+            project.log(e.getMessage());
+        } catch (ClientProtocolException e) {
+            project.log(e.getMessage());
+        } catch (IOException e) {
+            project.log(e.getMessage());
+        }
+        return deploy;
+    }
+}
